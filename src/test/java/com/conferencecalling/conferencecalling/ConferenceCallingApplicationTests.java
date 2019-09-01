ad0866204a4a1b849a800393dc3fddafdbbf6d09
Brandon Seager <brandon.seager@identitygroup.com>
2019-09-01 12:45:48 -0500

    finished step 1

---
+++ src/test/java/com/conferencecalling/conferencecalling/ConferenceCallingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.conferencecalling.conferencecalling;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConferenceCallingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
